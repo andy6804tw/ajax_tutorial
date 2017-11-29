c447d67ab0f0606e6a93105f02306416e7352581
Yi Lin Tsai <andy6804tw@yahoo.com.tw>
2017-11-29 21:12:37 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -22,3 +22,38 @@ fetch('http://f57de011.ngrok.io/product/', {method: 'get'})
 }).catch(function(err) {
     // Error :(
 })
+
+
+//post
+const data={
+	"product_card_name":"伸縮扣環",
+	"product_card_detail": "NAC-85TH-SG",
+  "product_card_img": [{"img":"http:image"}],
+	"product_card_price":"6666.00",
+	"product_card_content":"<h1>header</h1><p>類似得程式碼</p>"
+}
+fetch('https://435640db.ngrok.io/api/product/card',{
+           method: 'POST',
+           headers: {
+               'Accept': 'application/json',
+               'Content-Type': 'application/json'
+           },
+           body: JSON.stringify(data)
+       }).then(function checkStatus(response) {
+           if (response.status >= 200 && response.status < 300) {
+               return response.json();
+           } else {
+               var error = new Error(response.statusText)
+               error.response = response;
+               throw error;
+           }
+       })
+       .then(function(data) {
+          console.log("done")
+       	//完成
+       }).catch(function(error) {
+           console.log('request failed', error);
+           return error.response.json();
+       }).then(function(errorData){
+          //失敗
+       });
